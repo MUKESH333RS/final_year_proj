db80f610349fbf818dfffc1f29441a90c3de0e7f
MUKESH333RS <143300934+MUKESH333RS@users.noreply.github.com>
2024-09-01 08:34:51 +0530

    Add files via upload

---
+++ build/precache-manifest.2a8492d2c4facc53aa873992fff31d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4cd2b69e622befa55259af78b7dba5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4febd25e9b262f788f",
+    "url": "/static/css/main.d875e3f3.chunk.css"
+  },
+  {
+    "revision": "0344b5d76c920149eedf",
+    "url": "/static/js/2.a91700a4.chunk.js"
+  },
+  {
+    "revision": "4d4febd25e9b262f788f",
+    "url": "/static/js/main.8709f331.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
